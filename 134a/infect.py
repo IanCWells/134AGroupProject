167ca9beb84c69e56005158816f25096433d1aa6
pinklz <liz.manka@yahoo.com>
2024-05-10 15:37:23 -0700

    Add infect and infect_step functions in infect.py

--- 134a/infect.py
+++ 134a/infect.py
@@ -4,17 +4,33 @@ import random
 def infect_step(G,p1,individuals,N):
     '''The function serves as the infection model for each day.
     input params (consistent with the project description):
+
+    
     G (ndarray N*N): the adjacency matrix.
     p1: the probability each individual infects neighbours.
+    individuals = who's already infected
+    N = # of people in 'individuals'
     '''
 
     ###################################################
     '''your code here'''
+    individuals_updated = np.copy(individuals)
+    for i in range(N):
+        if individuals[i] == 1:     # this individual is infected
+            ''' Need to find all of their neighbors and infect them w/
+                probability p1 '''
+            for neighbor_indx in range(N):
+                if G[i][neighbor_indx] == 1:    # if i is neighbors w/ neighbor_indx
 
-                        
+                    if individuals_updated[neighbor_indx] == 0: # if this person is NOT already infected
+                        individuals_updated[neighbor_indx] = np.random.choice([0,1], p=[1-p1, p1])
+
+                    # for debugging purposes:
+                    #if individuals_updated[neighbor_indx] == 1:
+                     #   print("\tInfecting ", neighbor_indx, ", who is a neighbor of ", i)
+        
     ###################################################
     return individuals_updated
-
 
 
 
@@ -30,7 +46,15 @@ def infect(G,p0,p1,time_steps):
     individuals = np.zeros(N)
     ###################################################
     '''your code here'''
-
+    for i in range(N):
+        # infect everyone w/ initial probability p0
+        individuals[i] = np.random.choice([0,1], p=[1-p0, p0])
+    print("Original infected individuals: ", individuals)
+    
+    for _ in range(time_steps):
+        individuals = infect_step(G, p1, individuals, N)
+        print("Infected individuals after step ", _, " - ", individuals)
+        
     ###################################################
 
     return individuals
