ee98bbc9e352a556b89325b448b58e4d5607d6ae
IanCWells <ianchristopherwells@gmail.com>
2024-05-10 15:44:08 -0700

    sbm.py

--- 134a/sbm.py
+++ 134a/sbm.py
@@ -4,7 +4,8 @@ import networkx as nx
 def SBM(N,M,q0,q1):
     '''This function is designed to generate the Stochastic Block Model.
     input params (consistent with the project description):
-    sizes (list of int): the sizes of the communities
+    N (int): the number of individuals
+    M (int): the number of subgroups
     q0 (float): probability of inter-subgroup connection
     q1 (float): probability of intra-subgroup connection
 
@@ -13,7 +14,34 @@ def SBM(N,M,q0,q1):
     '''
     #################################################
     ''' your code here'''
+    G = np.zeros((N,N), dtype=int)
 
+    '''
+    i % size = index into subgroup
+    '''
+
+    size = round(N/M)
+    
+    #Testing
+    #print("Group size: ", size)
+
+   #  '''
+    for i in range(N):
+        i_subgroup = np.floor((i)/size)       # which subgroup this index is in
+        for j in range(N):
+            if i == j:
+                continue
+            
+            j_subgroup = np.floor((j)/size)
+            
+            if (j_subgroup == i_subgroup):
+                connected = np.random.choice([0,1], p=[1-q0, q0])   # for inter-group
+            else:
+                connected = np.random.choice([0,1], p=[1-q1, q1])   # for intra-group
+
+            G[i][j] = connected
+            G[j][i] = connected
+    # '''
     #################################################
 
     return G
