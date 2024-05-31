13dfec8988d2b3b30eb192b125434feb6fe59491
pinklz <liz.manka@yahoo.com>
2024-05-30 18:38:04 -0700

    Add gitignore file and updated algos and plot infection functions

--- 134a/main_functions.py
+++ 134a/main_functions.py
@@ -35,20 +35,27 @@ def iter(N,M,q0,q1,p0,p1,time_steps,num_sims,method,dataset='sbm'):
         if dataset=='sbm':
             for i in range(num_sims):
                 Gs[i] = SBM(N,M,q0,q1)
+                ###################################################
+                '''your code here'''
+                ###################################################
                 Individuals[i] = infect(Gs[i],p0,p1,time_steps)
                 s = Individuals[i]
-                Communities[i] = create_communities(s, N,M)
+                Communities[i] = create_communities(s,N,M)
         elif dataset=='iid':
             for i in range(num_sims):
+                ###################################################
+                '''your code here'''
                 individuals = np.random.choice([0, 1], size=N, p=[1 - p0, p0])
                 Individuals[i] = individuals
                 s = Individuals[i]
                 Communities[i] = create_communities(s, N,M)
+                ###################################################
         data['graph'] = Gs
         data['communities'] = Communities
         data['individuals'] = Individuals
         with open(name, 'wb') as infile:
             pickle.dump(data,infile) 
+    #         print('Dataset done!')
 
 
     ###################################################
@@ -58,13 +65,22 @@ def iter(N,M,q0,q1,p0,p1,time_steps,num_sims,method,dataset='sbm'):
     fraction_ppl_in_family = 0
     num_tests = 0
     num_stages = 0
+
+    frac_total_infected = 0
+    
+    total_infected_communities = 0
+    tot_avg = 0
+    
     ###################################################
         
     if os.path.isfile(name):
         with open(name, 'rb') as infile:
             data = pickle.load(infile) 
-        print('Data loaded!')
+       # print('Data loaded!')
     for i in range(num_sims): 
+        avg1 = 0
+        total_infected_communities_thiscycle = 0
+        
         '''
         if dataset=='synthetic':
             G = data['graph'][i]
@@ -73,73 +89,63 @@ def iter(N,M,q0,q1,p0,p1,time_steps,num_sims,method,dataset='sbm'):
         '''
         ###################################################
         '''your code to calculate the statistics here''' 
-        if(method == "binary"):
-            '''
-            If dataset = iid
-                Then the 'individuals' will be laid out in a randomized way
-            If the dataset = sbm
-                The 'individuals' are organized such that they are next to members of their community
-                B/c within 'infect', the rows of G are organized in such a fashion
-            '''
-            G = data['graph']
-            communities = data['communities'][i]
-            individuals = data['individuals'][i]
-            numtests_bs, num_stages_bs, _ = binary_splitting(individuals)
+        communities = data['communities'][i]
+        individuals = data['individuals'][i]
+
+        
+        frac_total_infected += float(float(num_infected(individuals))/float(len(individuals)))
+    
+
+        #Should be all infected communities in entire simulation
+        for community in communities:
+            if num_infected(community) > 0:
+                total_infected_communities += 1
+                total_infected_communities_thiscycle += 1
+
+
+        for community in communities:
+            # Yields a list of people in that particular community on this simulation
+            family_infections = num_infected(community)
+            if(family_infections > 0):
+                avg1 += (family_infections / len(community))
+
+        if(total_infected_communities_thiscycle > 0):
+            avg1 = avg1 / total_infected_communities_thiscycle
+            tot_avg += avg1
+
+
+        if(method == "binary"):            
+            s = individuals.copy()
+            if dataset == 'sbm':
+                np.random.shuffle(s)
+
+            numtests_bs, num_stages_bs, _ = binary_splitting(s)
             num_tests += numtests_bs
             num_stages += num_stages_bs
         elif(method == "Q1"):   # iid
-            G = data['graph']
-            communities = data['communities'][i]
-            individuals = data['individuals'][i]
             numtests_q1, num_stages_q1 = Qtesting1(individuals)
             num_tests += numtests_q1
             num_stages += num_stages_q1
         elif(method == "Q2"):   # iid
-            G = data['graph']
-            communities = data['communities'][i]
-            individuals = data['individuals'][i]
             numtests_q2, num_stages_q2 = Qtesting2(individuals)
             num_tests += numtests_q2
             num_stages += num_stages_q2
         elif(method == "diag"):
-            G = data['graph']
-            communities = data['communities'][i]
-            individuals = data['individuals'][i]
-            numtests_diag, num_stages_diag = diag_splitting(individuals)
+            s = individuals.copy()
+            if dataset == 'sbm':
+                np.random.shuffle(s)
+            
+            numtests_diag, num_stages_diag = diag_splitting(s)
             num_tests += numtests_diag
             num_stages += num_stages_diag
         elif(method == "comm1"):    # sbm
-            G = data['graph']
-            communities = data['communities'][i]
-            individuals = data['individuals'][i]
             numtests_comm1, num_stages_comm1 = Q1_commaware(communities)
             num_tests += numtests_comm1
             num_stages += num_stages_comm1
         elif(method == "comm2"):    # sbm
-            G = data['graph']
-            communities = data['communities'][i]
-            individuals = data['individuals'][i]
             numtests_comm2, num_stages_comm2 = Q2_commaware(communities)
             num_tests += numtests_comm2
             num_stages += num_stages_comm2
-
-        ###################################################
-
-        # interleave the individuals
-        #s = Individuals.copy()
-        #np.random.shuffle(s)
-        # binary
-       # numtests_bs, num_stages_bs, _ = binary_splitting(s)
-        '''
-        # algorithm 1
-        numtests_q1, num_stages_q1 = Qtesting1(s)
-        # algorithm 2
-        numtests_q2, num_stages_q2 = Qtesting2(s)
-        # community-aware
-        numtests_q1_c, num_stages_q1_c = Qtesting1_comm_aware(Individuals.copy(),communities)
-        # community-aware
-        numtests_q2_c, num_stages_q2_c = Qtesting2_comm_aware(Individuals.copy(),communities)
-        '''
 
     ###################################################
     '''your code to calculate the statistics here''' 
@@ -147,117 +153,98 @@ def iter(N,M,q0,q1,p0,p1,time_steps,num_sims,method,dataset='sbm'):
     num_tests /= num_sims
     num_stages /= num_sims
 
-    ###################################################
+    #s = Individuals.copy()
+    #num_infected(s)
+    #Should average all infected people over total number of people in all simulations 
+
+    #Somehow it is only looping through a part of the population??
+    #print(frac_total_infected)
+    
+    #Problem is not num_sims
+    fraction_ppl = float(frac_total_infected) / float(num_sims)
+
+    #Need to figure out how to establish communities properly
+    #Fraction family: average infected communities / total communities as a percentage
+    fraction_family = total_infected_communities / (M * num_sims)
+    
+    #fraction_ppl_in_family:  is the number of people infected in a family / total size of family (averaged)
+    
+    fraction_ppl_in_family = tot_avg / (num_sims)
             
 
     return fraction_ppl, fraction_family, fraction_ppl_in_family, num_tests, num_stages
         
 
-def plot_results(N, M, q0, q1, p1, time_steps, num_sims, method, method2, method3, method4,method5, method6, p_values):
-    num_tests_list = []
-    num_stages_list = []
 
-    num_tests_list2 = []
-    num_stages_list2 = []
+def plot_infect(N, M, p0, time_steps, num_sims, method):
+    # Define the different q0 and q1 pairs
+    q_values = [
+        (1, 0),
+        (0.9, 0.1),
+        (0.5, 0.3)
+    ]
 
-    num_tests_list3 = []
-    num_stages_list3 = []  
+    # Define the p1 values to iterate over
+    p1_values = np.linspace(0.05, 1, 9)
 
-    num_tests_list4 = []
-    num_stages_list4 = []
+    # Initialize lists to hold the results for each q_value pair
+    all_frac_infected = []
+    all_frac_communities = []
+    all_frac_inCommunity = []
 
-    num_tests_comm1 = []
-    num_stages_comm1 = []
+    # Iterate over each q0, q1 pair and collect the data
+    for (q0, q1) in q_values:
+        frac_infected_list = []
+        frac_infected_communities = []
+        frac_infected_inCommunity = []
 
-    num_tests_comm2 = []
-    num_stages_comm2 = []  
-    for p0 in p_values:
-        _, _, _, num_tests, num_stages = iter(N, M, q0, q1, p0, p1, time_steps, num_sims, method, dataset='sbm')
-        num_tests_list.append(num_tests)
-        num_stages_list.append(num_stages)
+        for p1 in p1_values:
+            frac_infected, frac_communities, frac_inCommunity, _, _ = iter(N, M, q0, q1, p0, p1, time_steps, num_sims, method, dataset='sbm')
+            frac_infected_list.append(frac_infected)
+            frac_infected_communities.append(frac_communities)
+            frac_infected_inCommunity.append(frac_inCommunity)
 
-    for p0 in p_values:
-        _, _, _, num_tests, num_stages = iter(N, M, q0, q1, p0, p1, time_steps, num_sims, method2, dataset='sbm')
-        num_tests_list2.append(num_tests)
-        num_stages_list2.append(num_stages)
+        all_frac_infected.append(frac_infected_list)
+        all_frac_communities.append(frac_infected_communities)
+        all_frac_inCommunity.append(frac_infected_inCommunity)
 
-    for p0 in p_values:
-        _, _, _, num_tests, num_stages = iter(N, M, q0, q1, p0, p1, time_steps, num_sims, method3, dataset='sbm')
-        num_tests_list3.append(num_tests)
-        num_stages_list3.append(num_stages)     
-
-    for p0 in p_values:
-        _, _, _, num_tests, num_stages = iter(N, M, q0, q1, p0, p1, time_steps, num_sims, method4, dataset='sbm')
-        num_tests_list4.append(num_tests)
-        num_stages_list4.append(num_stages) 
-
-    # Community testing 1
-    for p0 in p_values:
-        _, _, _, num_tests, num_stages = iter(N, M, q0, q1, p0, p1, time_steps, num_sims, method5, dataset='sbm')
-        num_tests_comm1.append(num_tests)
-        num_stages_comm1.append(num_stages)      
-    plt.figure(figsize=(18, 8))
-    
-    # Plot for number of tests
-    plt.subplot(1, 2, 1)
-    plt.plot(p_values, num_tests_list, marker='o', label=method)
-    plt.plot(p_values, num_tests_list2, marker='x', label=method2)
-    plt.plot(p_values, num_tests_list3, marker='^', label=method3)
-    plt.plot(p_values, num_tests_list4, marker='^', label=method4)
-    plt.plot(p_values, num_tests_comm1, marker='p', label=method5)
-    plt.title('Number of Tests vs p0')
-    plt.xlabel('p0')
-    plt.ylabel('Number of Tests')
+    # Plotting frac_infected for each q0, q1 pair
+    plt.figure(figsize=(8,6))
+    for i, (q0, q1) in enumerate(q_values):
+        plt.plot(p1_values, all_frac_infected[i], marker='o', linestyle='-', label=f'q0={q0}, q1={q1}')
+    plt.title('Fraction of Infected Individuals vs p1')
+    plt.xlabel('p1')
+    plt.ylabel('Fraction of Infected Individuals')
     plt.legend()
-    
-    # Plot for number of stages
-    plt.subplot(1, 2, 2)
-    plt.plot(p_values, num_stages_list, marker='o', label=method)
-    plt.plot(p_values, num_stages_list2, marker='x', label=method2)
-    plt.plot(p_values, num_stages_list3, marker='^', label=method3)
-    plt.plot(p_values, num_stages_list4, marker='^', label=method4)
-    plt.plot(p_values, num_stages_comm1, marker='p', label=method5)
-    plt.title('Number of Stages vs p0')
-    plt.xlabel('p0')
-    plt.ylabel('Number of Stages')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    # Plotting frac_communities for each q0, q1 pair
+    plt.figure(figsize=(8,6))
+    for i, (q0, q1) in enumerate(q_values):
+        plt.plot(p1_values, all_frac_communities[i], marker='o', linestyle='-', label=f'q0={q0}, q1={q1}')
+    plt.title('Fraction of Infected Communities vs p1')
+    plt.xlabel('p1')
+    plt.ylabel('Fraction of Infected Communities')
     plt.legend()
-    
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+    # Plotting frac_inCommunity for each q0, q1 pair
+    plt.figure(figsize=(8,6))
+    for i, (q0, q1) in enumerate(q_values):
+        plt.plot(p1_values, all_frac_inCommunity[i], marker='o', linestyle='-', label=f'q0={q0}, q1={q1}')
+    plt.title('Fraction of Infected Individuals within Communities vs p1')
+    plt.xlabel('p1')
+    plt.ylabel('Fraction of Infected Individuals within Communities')
+    plt.legend()
+    plt.grid(True)
     plt.tight_layout()
     plt.show()
 
 
-def plot_infect(N, M, q0, q1, p1, time_steps, num_sims, method, values):
-    frac_infected_list = []
-    frac_infected_communities = []
-    frac_infected_inCommunity = []
-
-    for p0 in values:
-        frac_infected, frac_communities, frac_inCommunity, _, _ = iter(N, M, q0, q1, p0, p1, time_steps, num_sims, method, dataset='iid')
-        frac_infected_list.append(frac_infected)
-        frac_infected_communities.append(frac_communities)
-        frac_infected_inCommunity.append(frac_inCommunity)
-
-    #fig, axs = plt.subplots(3, 1, figsize=(10, 15))
-
-    ''' 
-    axs[0].plot(values, frac_infected_list, marker='o', linestyle='-', color='b')
-    axs[0].set_title('Fraction of Infected Individuals vs p0')
-    axs[0].set_xlabel('p0')
-    axs[0].set_ylabel('Fraction of Infected Individuals')
-
-    axs[1].plot(values, frac_infected_communities, marker='o', linestyle='-', color='g')
-    axs[1].set_title('Fraction of Infected Communities vs p0')
-    axs[1].set_xlabel('p0')
-    axs[1].set_ylabel('Fraction of Infected Communities')
-
-    axs[2].plot(values, frac_infected_inCommunity, marker='o', linestyle='-', color='r')
-    axs[2].set_title('Fraction of Infected Individuals within Communities vs p0')
-    axs[2].set_xlabel('p0')
-    axs[2].set_ylabel('Fraction of Infected Individuals within Communities')
-
-    plt.tight_layout()
-    plt.show() 
-    '''
 
 def plot_iid(N, M, q0, q1, p1, time_steps, num_sims, binary, diag, testing1, testing2, p_values):
     num_tests_binary_iid = []
@@ -301,7 +288,7 @@ def plot_iid(N, M, q0, q1, p1, time_steps, num_sims, binary, diag, testing1, tes
     
     # Plot for number of tests
     plt.subplot(1, 2, 1)
-    #plt.plot(p_values, num_tests_binary_iid, marker='o', label=binary)
+    plt.plot(p_values, num_tests_binary_iid, marker='o', label=binary)
     plt.plot(p_values, num_tests_diag_iid, marker='x', label=diag)
     plt.plot(p_values, num_tests_q1, marker='^', label=testing1)
     plt.plot(p_values, num_tests_q2, marker='^', label=testing2)
@@ -313,7 +300,7 @@ def plot_iid(N, M, q0, q1, p1, time_steps, num_sims, binary, diag, testing1, tes
     
     # Plot for number of stages
     plt.subplot(1, 2, 2)
-    #plt.plot(p_values, num_stages_binary_iid, marker='o', label=binary)
+    plt.plot(p_values, num_stages_binary_iid, marker='o', label=binary)
     plt.plot(p_values, num_stages_diag_iid, marker='x', label=diag)
     plt.plot(p_values, num_stages_q1, marker='^', label=testing1)
     plt.plot(p_values, num_stages_q2, marker='^', label=testing2)
@@ -325,6 +312,8 @@ def plot_iid(N, M, q0, q1, p1, time_steps, num_sims, binary, diag, testing1, tes
     
     plt.tight_layout()
     plt.show()
+
+
 
 def plot_comms(N, M, q0, q1, p0, time_steps, num_sims, binary, diag, testingcomm1, testingcomm2, p_values):
     num_tests_binary = []
@@ -362,9 +351,9 @@ def plot_comms(N, M, q0, q1, p0, time_steps, num_sims, binary, diag, testingcomm
     
     # Plot for number of tests
     plt.subplot(1, 2, 1)
-    #plt.plot(p_values, num_tests_binary, marker='o', label=binary)
+    plt.plot(p_values, num_tests_binary, marker='o', label=binary)
     plt.plot(p_values, num_tests_diag, marker='x', label=diag)
-    plt.plot(p_values, num_tests_comm1, marker='p', label=testingcomm1)
+    plt.plot(p_values, num_tests_comm1, marker='^', label=testingcomm1)
     plt.plot(p_values, num_tests_comm2, marker='o', label=testingcomm2)
     plt.title('Number of Tests vs p1')
     plt.xlabel('p1')
@@ -373,9 +362,9 @@ def plot_comms(N, M, q0, q1, p0, time_steps, num_sims, binary, diag, testingcomm
     
     # Plot for number of stages
     plt.subplot(1, 2, 2)
-   # plt.plot(p_values, num_stages_binary, marker='o', label=binary)
+    plt.plot(p_values, num_stages_binary, marker='o', label=binary)
     plt.plot(p_values, num_stages_diag, marker='x', label=diag)
-    plt.plot(p_values, num_stages_comm1, marker='p', label=testingcomm1)
+    plt.plot(p_values, num_stages_comm1, marker='^', label=testingcomm1)
     plt.plot(p_values, num_stages_comm2, marker='o', label=testingcomm2)
     plt.title('Number of Stages vs p1')
     plt.xlabel('p1')
